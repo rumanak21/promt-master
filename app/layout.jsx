c8080bfa3deee39eb84ce226b253e6aaab701a32
rumanak21 <rumana2k20@gmail.com>
2023-08-28 02:08:39 +0200

    promt-master project setup

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/globals.css'
+import { Children } from 'react'
+
+export const metadata = {
+    tittle: "Promtmaster",
+    description: "Discover & Share AI Prompts"
+}
+
+const RootLayout = ({children}) => {
+    return (
+
+        <html lang="en">
+            <body>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
+                <main className='app'>
+                    {children}
+                </main>
+
+
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout

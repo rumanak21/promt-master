a906bc9c5201cfec98fb29b75772792626e2274b
rumanak21 <rumana2k20@gmail.com>
2023-09-18 05:52:38 +0200

    Modify some pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,26 @@
 import '@styles/globals.css'
-import { Children } from 'react'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
-    tittle: "Promtmaster",
+    tittle: "Promt-master",
     description: "Discover & Share AI Prompts"
 }
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
     return (
 
         <html lang="en">
             <body>
-                <div className='main'>
-                    <div className='gradient' />
-                </div>
-                <main className='app'>
-                    {children}
-                </main>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
 
 
             </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,17 @@
+import Feed from '@components/Feed';
 
 const Home = () => {
   return (
-    <section>
-        
+    <section className="w-full flex-center flex-col "> 
+        <h1 className=" head_text text-center ">
+          Discover & Share <span/>
+          <br className=" max-md:hidden " />
+          <span className=" orange_gradient tex-center ">AI-Power Prompts </span>
+        </h1>
+        <p className=" desc text-center ">
+          Prompt-Master is an open source AI prompting tool for modern world to discover, create and share creative prompts.
+        </p>
+        <Feed/>
     </section>
   )
 }
